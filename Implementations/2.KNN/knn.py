1143371fff91e362f99c3ee9390b2d136a7b5f0a
Saurabh Rathi <saurabhrathi12@gmail.com>
2017-02-19 19:09:06 +0530

    knn implementation complete

--- Implementations/2.KNN/knn.py
+++ Implementations/2.KNN/knn.py
@@ -7,27 +7,47 @@ data=[]
 for row in csv_file_object:
 	data.append(row)
 
-#Splitting data into training,test,crossvalidation data 
 data=np.array(data)
 data=data[2::]
 x=data[:,0:24]
-y=data[:,24]
+y=data[:,24:25]
 x=x[:,:].astype(np.float64)
 x=(x-np.mean(x,axis=0))/np.std(x,axis=0)
 y=y[:].astype(np.float64)
-#y.resize((30000,1))
 trainx=x[0:21000]
 trainy=y[0:21000]
 remainx=x[21000:30000]
 remainy=y[21000:30000]
-crossvalidatex=x[21000:25500]
-crossvalidatey=y[21000:25500]
-testx=x[25500:30000]
-testy=y[25500:30000]
 
-#Implementation
-from sklearn.neighbors import KNeighborsClassifier
-for i in range(1,15):
-	neigh = KNeighborsClassifier(n_neighbors=i)
-	neigh.fit(trainx, trainy)
-	print neigh.score(remainx,remainy)
+distance0=[]
+distance1=[]
+for j in range(len(remainx)):
+	d0=[]
+	d1=[]
+	print j
+	for i in range(len(trainx)):
+		temp=(np.square(trainx[i]-remainx[j])).sum()
+		if(trainy[i]==0):
+			d0.append(temp)
+		else:
+			d1.append(temp)
+	d0.sort()
+	d1.sort()
+	distance0.append(d0)
+	distance1.append(d1)
+distance0=np.array(distance0)
+distance1=np.array(distance1)
+for i in range(1,51):
+	distance0[:,i:i+1]=distance0[:,i:i+1]+distance0[:,i-1:i]
+	distance1[:,i:i+1]=distance1[:,i:i+1]+distance1[:,i-1:i]
+for i in range(0,50):
+	sum=0
+	for j in range(len(remainx)):
+		if(distance0[j][i]>distance1[j][i]):
+			if(remainy[j]==1):
+				sum=sum+1
+		else:
+			if(remainy[j]==0):
+				sum=sum+1
+	print "k = ",i+1," Accuracy = ",(sum*1.0)/len(remainy)
+
---
+++ Implementations/2.KNN/sklearnknn.py
@@ -0,0 +1,33 @@
+import os,csv,math
+import numpy as np
+
+#Reading Data
+csv_file_object = csv.reader(open('csvdataset.csv', 'rb'))
+data=[]
+for row in csv_file_object:
+	data.append(row)
+
+#Splitting data into training,test,crossvalidation data 
+data=np.array(data)
+data=data[2::]
+x=data[:,0:24]
+y=data[:,24]
+x=x[:,:].astype(np.float64)
+x=(x-np.mean(x,axis=0))/np.std(x,axis=0)
+y=y[:].astype(np.float64)
+#y.resize((30000,1))
+trainx=x[0:21000]
+trainy=y[0:21000]
+remainx=x[21000:30000]
+remainy=y[21000:30000]
+crossvalidatex=x[21000:25500]
+crossvalidatey=y[21000:25500]
+testx=x[25500:30000]
+testy=y[25500:30000]
+
+#Implementation
+from sklearn.neighbors import KNeighborsClassifier
+for i in range(1,51):
+	neigh = KNeighborsClassifier(n_neighbors=i)
+	neigh.fit(trainx, trainy)
+	print i,neigh.score(remainx,remainy)
